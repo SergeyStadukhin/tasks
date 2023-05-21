05920efb2206f18f5f4b5c6c474de60c611d05ff
Sergei <amatory365@gmail.com>
2023-05-21 18:08:02 +0300

    freeCodeCamp

--- tasks.js
+++ tasks.js
@@ -102,3 +102,94 @@ function timesFive(num) {
 }
 
 const answer = timesFive(5);
+
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+
+
+function myLocalScope() {
+  // Only change code below this line
+let myVar = 0
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum+=5
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7)
+
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if(wasThatTrue){
+    return "Yes, that was true"
+  }
+  return "No, that was false"
+
+
+  // Only change code above this line
+
+}
+
+
+// Setup
+function testEqual(val) {
+  if (val===12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+
+
