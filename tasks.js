16f11dd7a371bbf572d8fb4d9013fab54d459973
Sergei <amatory365@gmail.com>
2023-05-21 18:51:29 +0300

    freeCodeCamp

--- tasks.js
+++ tasks.js
@@ -192,4 +192,337 @@ function testEqual(val) {
 testEqual(10);
 
 
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+
+function testLessThan(val) {
+  if (val<25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val<55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+
+function testLessOrEqual(val) {
+  if (val<=12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val<=24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val>=25 && val <=50) {
+    return 'Yes'
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val<10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else result = "5 or Smaller"; 
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+
+function testSize(num) {
+  // Only change code below this line
+  if(num<5){
+    return "Tiny"
+  } else if(num<10){
+    return "Small"
+  } else if(num<15){
+    return "Medium"
+  } else if(num<20){
+    return "Large"
+  } else if(num>=20){
+    return "Huge"
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
+
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if(strokes == 1) {
+    return "Hole-in-one!"
+  } else if(strokes <=par-2){
+    return "Eagle"
+  } else if(strokes ==par-1){
+    return "Birdie"
+  } else if(strokes ==par){
+    return "Par"
+  }else if(strokes ==par + 1){
+    return "Bogey"
+  }else if(strokes ==par + 2){
+    return "Double Bogey"
+  }else if(strokes >= par + 3){
+    return "Go Home!"
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1: 
+      answer = 'alpha';
+      break
+    case 2: 
+      answer = 'beta';
+      break
+    case 3: 
+      answer = 'gamma';
+      break
+    case 4: 
+      answer = 'delta';
+      break
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 'a':
+      answer = 'apple'
+      break
+    case 'b':
+      answer = 'bird'
+      break
+    case 'c':
+      answer = 'cat'
+      break
+    default:
+      answer = 'stuff'
+      break
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low"
+      break
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid"
+      break
+    case 7:
+    case 8:
+    case 9:
+      answer = "High"
+      break
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case "bob":
+      answer = "Marley";
+      break
+    case 42:
+      answer = "The Answer"
+      break
+    case 1:
+      answer = "There is no #1"
+      break;
+    case 99:
+      answer = "Missed me by this much!"
+      break
+    case 7:
+      answer = "Ate Nine"
+      break
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+
+  if(a<0 || b< 0) return undefined
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1
+      break
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count -=1
+      break
+    }
+    return `${count} ${count>0 ? 'Bet' : "Hold"}`
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+const myDog = {
+  // Only change code below this line
+  name: '',
+  legs: 0,
+  tails: 0,
+  friends: []
+
+  // Only change code above this line
+};
+
+
+
 
