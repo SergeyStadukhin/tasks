ab76b908d7581b0050907a5d9cd9de47835dd120
Sergei <amatory365@gmail.com>
2023-05-21 17:31:24 +0300

    FreeCodeCamp

--- tasks.js
+++ tasks.js
@@ -61,3 +61,27 @@ function SeriesSum(n)
   return sum
 }
 // console.log(SeriesSum(4));
+
+// freeCodeCamp
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift()
+
+//
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35])
+
+const myList = [];
+
+myList.push(["Chocolate Bar", 15])
+myList.push(["Chocolate Bar", 15])
+myList.push(["Chocolate Bar", 15])
+myList.push(["Chocolate Bar", 15])
+myList.push(["Chocolate Bar", 15])
+
